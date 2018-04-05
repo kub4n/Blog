9565459406d3b56abab1d0256aa822164ff463b5
kub4n <j.solecki@moonbite.pl>
2018-04-05 21:55:30 +0200

    initial

---
+++ Transformers/FullPostTransformer.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Modules\Blog\Transformers;
+
+use Illuminate\Http\Resources\Json\Resource;
+use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
+
+class FullPostTransformer extends Resource
+{
+    public function toArray($request)
+    {
+        $postData = [
+            'id' => $this->id,
+            'title' => $this->title,
+            'slug' => $this->slug,
+            'category_id' => $this->category_id
+        ];
+        foreach (LaravelLocalization::getSupportedLocales() as $locale => $supportedLocale) {
+            $postData[$locale] = [];
+            foreach ($this->translatedAttributes as $translatedAttribute) {
+                $postData[$locale][$translatedAttribute] = $this->translateOrNew($locale)->$translatedAttribute;
+            }
+        }
+
+        foreach ($this->tags as $tag) {
+            $postData['tags'][] = $tag->name;
+        }
+        return $postData;
+    }
+}
